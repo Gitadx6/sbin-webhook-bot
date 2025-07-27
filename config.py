d14e71ee874d45b5515f1ed6c45642a0a792a83f
Gitadx6 <gd.official.mail@gmail.com>
2025-07-27 19:00:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,13 @@ WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
 kite = KiteConnect(api_key=API_KEY)
 kite.set_access_token(ACCESS_TOKEN)
 
+def resolve_token(symbol):
+    instruments = kite.instruments("NFO")
+    for item in instruments:
+        if item["tradingsymbol"] == symbol:
+            return item["instrument_token"]
+    raise Exception(f"Instrument token not found for symbol: {symbol}")
+
 # Global position tracker
 current_position = {
     "symbol": None,
